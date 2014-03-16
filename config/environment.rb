ba4a928ba3c51a5b58bdaa384aeade48f91d41a8
Mark Oleson <fusion2004@gmail.com>
2014-03-16 00:36:10 -0400

    basic footer with copyright and versioning information

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Compo::Application.initialize!
+
+APP_VERSION = `git describe --always --tags` unless defined? APP_VERSION
