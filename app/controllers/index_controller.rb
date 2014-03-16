2eb9dd45415a19f7dc2ff9c1acd6524267a0bca4
Mark Oleson <fusion2004@gmail.com>
2014-03-16 09:18:20 -0400

    setup route just for causing an exception, installed rollbar

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,8 @@
 class IndexController < ApplicationController
   def index
   end
+
+  def error
+    Rails.what_now_errors
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Compo::Application.routes.draw do
   devise_for :users
+  get 'error', to: 'index#error'
   root to: 'index#index'
 end
