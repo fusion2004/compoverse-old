b676edcf28957d6bacb368df3c6762603d141966
Mark Oleson <fusion2004@gmail.com>
2014-03-16 09:37:12 -0400

    updating changelog, bumping version

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Compo::Application.initialize!
 
-APP_VERSION = "1.2.4" unless defined? APP_VERSION
+APP_VERSION = "1.2.5" unless defined? APP_VERSION
