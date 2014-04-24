8ccead99b9a716b650d6a2c63e73b97f51a51176
Mark Oleson <fusion2004@gmail.com>
2014-04-23 23:58:00 -0400

    style guide fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,8 +42,8 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
-  config.include Devise::TestHelpers, :type => :controller
-  config.extend ControllerMacros, :type => :controller
+  config.include Devise::TestHelpers, type: :controller
+  config.extend ControllerMacros, type: :controller
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
