2fa116d65f7a24e53e5769f5448712ced72ec8e1
Mark Oleson <fusion2004@gmail.com>
2014-04-23 23:44:03 -0400

    fixed controller spec

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -1,8 +1,18 @@
 require "spec_helper"
 
 describe Admin::DashboardController do
+  login_admin
+
   describe "GET index" do
-    get :index
-    expect(response).to render_template("index")
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 end
