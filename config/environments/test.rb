a804c26747a12d523e3dec0f490124ab0708f3e8
Mark Oleson <fusion2004@gmail.com>
2014-05-21 13:10:03 -0400

    rename the app to ThaSauceCompo: d'oh name collision

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Compo::Application.configure do
+ThaSauceCompo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
