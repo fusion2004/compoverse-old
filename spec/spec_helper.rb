6bc114a26acbee72937aad34772053a533956f69
Mark Oleson <fusion2004@gmail.com>
2014-08-09 04:36:06 -0400

    fix tests and quotes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,6 +42,7 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
+  config.infer_spec_type_from_file_location!
   config.include Devise::TestHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
 
